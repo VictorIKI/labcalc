22233820e03a1d18334f394e9bef69a217341d5b
VictorIKI <54743939+VictorIKI@users.noreply.github.com>
2020-01-27 14:43:09 +0100

    Update ising.c

--- ising.c
+++ ising.c
@@ -5,14 +5,18 @@
 
 #define N 12
 #define N2 4096
+#define M1 1000
+#define M2 10000
 
 //void genconf(int conf[],double energie1[])
 double epsilon(int conf[]);
+void spinconf(int conf[]);
 
 int main(){
-  int conf[N]={0},controllo[2*N+1]={-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12},frequenza[2*N+1];
-  double energie1[N2]={0};
+  int conf[N]={0},controllo[2*N+1]={-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12},frequenza[2*N+1],M;
+  double energie1[N2]={0},energie2[10000]={0};;
   int i,i1,i2,i3,i4,i5,i6,i7,i8,i9,i10,i11,i12,iconf,j=0;
+  FILE* fp;
 
   //Blocco generazione 4096 configurazioni ed energie
   for(i1=1;i1<=2;i1++){
@@ -42,7 +46,7 @@ int main(){
                           //assegno le energie
                           energie1[iconf]=epsilon(conf);
                           //stampa delle prime dieci configuazioni
-                          if(j<N2){
+                          if(j<10){
                             printf("Configurazione n° %2i",j+1);
                             for (i=0;i<N;i++){
                               printf("|%2i",conf[i]);
@@ -64,6 +68,7 @@ int main(){
       }
     }
   }
+  //calcolo le frequenze
   for(i=0;i<N2;i++){
     for(j=0;j<2*N+1;j++){
       if(controllo[j]==energie1[i]){
@@ -72,9 +77,45 @@ int main(){
       }
     }
   }
+  //scrivo su file le frequenze
+  fp= fopen("histo1.dat","w+");
   for(j=0;j<2*N+1;j++){
-    printf("%i f= %i\n",controllo[j],frequenza[j]);
+    fprintf(fp,"%i %i\n",controllo[j],frequenza[j]);
   }
+  fclose(fp);
+
+  //chiedo il numero di configurazioni da generare
+  do{
+    printf("Inserire un numero di configurazioni da generare compreso tra %i e %i:",M1,M2);
+    scanf("%i",&M);
+  }while(M<M1||M>M2);
+  j=0;
+
+  //genero le configurazioni casuali e ne calcolo l'energia
+  for (i=0;i<M;i++){
+    spinconf(conf);
+    energie2[i]=epsilon(conf);
+    //stampo le prime 10 configurazioni
+    if(j<10){
+      printf("Configurazione n° %2i",j+1);
+      for (i1=0;i1<N;i1++){
+        printf("|%2i",conf[i1]);
+      }
+      printf("| eps: %lf\n",energie2[j]);
+      j++;
+    }
+  }
+
+  for(i=0;i<N2;i++){
+    for(j=0;j<2*N+1;j++){
+      if(controllo[j]==energie1[i]){
+        frequenza[j]++;
+
+      }
+    }
+  }
+
+
 }
 
 double epsilon(int conf[]){
@@ -88,6 +129,14 @@ double epsilon(int conf[]){
   }
   //eps*=(double)-1/N;
   return eps;
+}
+
+void spinconf(int conf[]){
+  int i,j;
+  for (i=0;i<N;i++){
+    j= rand()%2+1;
+    conf[i]=pow(-1.0,j);
+  }
 }
 
 /*void genconf(int conf[][N2],energie1[],double (*f)(int conf[][N2])){
@@ -104,7 +153,6 @@ double epsilon(int conf[]){
                     for(i10=1;i10<=2;i10++){
                       for(i11=1;i11<=2;i11++){
                         for(i12=1;i12<=2;i12++){
-
                             conf[0][iconf]=pow(-1.0,i1);
                             conf[1][iconf]=pow(-1.0,i2);
                             conf[2][iconf]=pow(-1.0,i3);
@@ -117,11 +165,8 @@ double epsilon(int conf[]){
                             conf[9][iconf]=pow(-1.0,i10);
                             conf[10][iconf]=pow(-1.0,i11);
                             conf[11][iconf]=pow(-1.0,i12);
-
                           energie1[iconf]=f(conf[][iconf]);
                           iconf++;
-
-
                         }
                       }
                     }
